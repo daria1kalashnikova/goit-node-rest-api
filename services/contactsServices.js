0ae1273658dda4ab0348989d21a0da2d38772ca4
daria1kalashnikova <dk.kalashnikova@gmail.com>
2025-04-02 17:56:12 +0300

    create model

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,50 +1,41 @@
-import fs from "node:fs/promises";
-import path from "node:path";
-import { nanoid } from "nanoid";
-
-const contactsPath = path.join(process.cwd(), "db", "contacts.json");
-const updateContacts = (contacts) =>
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+import User from "../db/models/Contact.js";
 
 export async function listContacts() {
-  const contacts = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(contacts);
+  // const contacts = await fs.readFile(contactsPath, "utf-8");
+  // return JSON.parse(contacts);
 }
 
 export async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contact = contacts.find((item) => item.id === contactId);
-  return contact || null;
+  // const contacts = await listContacts();
+  // const contact = contacts.find((item) => item.id === contactId);
+  // return contact || null;
 }
 
 export async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
-  if (index === -1) return null;
-  const [removedContact] = contacts.splice(index, 1);
-  await updateContacts(contacts);
-  return removedContact || null;
+  // const contacts = await listContacts();
+  // const index = contacts.findIndex((item) => item.id === contactId);
+  // if (index === -1) return null;
+  // const [removedContact] = contacts.splice(index, 1);
+  // await updateContacts(contacts);
+  // return removedContact || null;
 }
 
 export async function addContact(data) {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    ...data,
-  };
-  contacts.push(newContact);
-  await updateContacts(contacts);
-  return newContact;
+  // const contacts = await listContacts();
+  // const newContact = {
+  //   id: nanoid(),
+  //   ...data,
+  // };
+  // contacts.push(newContact);
+  // await updateContacts(contacts);
+  // return newContact;
 }
 
 export async function updateContact(id, data) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === id);
-  if (index === -1) return null;
-
-  contacts[index] = { ...contacts[index], ...data };
-
-  await updateContacts(contacts);
-
-  return contacts[index];
+  // const contacts = await listContacts();
+  // const index = contacts.findIndex((item) => item.id === id);
+  // if (index === -1) return null;
+  // contacts[index] = { ...contacts[index], ...data };
+  // await updateContacts(contacts);
+  // return contacts[index];
 }
